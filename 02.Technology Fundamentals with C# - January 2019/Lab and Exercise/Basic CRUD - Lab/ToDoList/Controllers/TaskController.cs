e8c8c379f734fbd3976c591b220dfc24d04012ff
KrastevIT <barcodebit000@gmail.com>
2019-04-03 13:53:30 +0300

    Add Basic CRUD - Lab

---
+++ 02.Technology Fundamentals with C# - January 2019/Lab and Exercise/Basic CRUD - Lab/ToDoList/Controllers/TaskController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class TaskController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (var db = new ToDoDbContext())
+            {
+                var allTasks = db.Tasks.ToList();
+                return View(allTasks);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string title, string comments)
+        {
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(comments))
+            {
+                return RedirectToAction("Index");
+            }
+            Task task = new Task
+            {
+                Title = title,
+                Comments = comments
+            };
+            using (var db = new ToDoDbContext())
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new ToDoDbContext())
+            {
+                var taskToEdit = db.Tasks.FirstOrDefault(t => t.Id == id);
+
+                if (taskToEdit == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View(taskToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task newTask)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            using (var db = new ToDoDbContext())
+            {
+                var taskToEdit = db.Tasks.FirstOrDefault(t => t.Id == newTask.Id);
+
+                if (taskToEdit == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                taskToEdit.Title = newTask.Title;
+                taskToEdit.Comments = newTask.Comments;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public IActionResult Details(int id)
+        {
+            using (var db = new ToDoDbContext())
+            {
+                var taskToEdit = db.Tasks.FirstOrDefault(t => t.Id == id);
+
+                if (taskToEdit == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View(taskToEdit);
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            using (var db = new ToDoDbContext())
+            {
+                var task = db.Tasks.FirstOrDefault(t => t.Id == id);
+
+                if (task == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                db.Tasks.Remove(task);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ 02.Technology Fundamentals with C# - January 2019/Lab and Exercise/Basic CRUD - Lab/ToDoList/Data/ToDoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class ToDoDbContext : DbContext
+    {
+        public virtual DbSet<Task> Tasks { get; set; }
+
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=ToDoListDb;Integrated Security=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
