a31592ce5ad0eaa83bfce1d5cab8eb40282cdbc4
KrastevIT <barcodebit000@gmail.com>
2019-04-06 19:43:02 +0300

    add Final Exam

---
+++ 02.Technology Fundamentals with C# - January 2019/Exam/Final Exam/(Demo) Technology Fundamentals Final Exam - 06 April 2019/02 Deciphering/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02_Deciphering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] symbols = Console.ReadLine().Split();
+
+            string regex = @"^(([d-z]+[{},|#]*)+)";
+
+            var decode = Regex.Matches(input, regex);
+
+            if (decode.Count == 0)
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+                return;
+            }
+
+            string start = decode[0].Value;
+
+            if (input.ToString() != start)
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+                return;
+            }
+
+            string valid = string.Empty;
+
+            foreach (var item in decode)
+            {
+                valid = item.ToString();
+            }
+
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < valid.Length; i++)
+            {
+                int by3Symbols = valid[i] - 3;
+
+                char point = (char)by3Symbols;
+                string letters = point.ToString();
+
+                builder.Append(letters);
+            }
+
+            string lettersSearch = symbols[0];
+            string lettersCorenct = symbols[1];
+
+            builder.Replace(lettersSearch, lettersCorenct);
+
+            Console.WriteLine(builder);
+
+        }
+    }
+}
