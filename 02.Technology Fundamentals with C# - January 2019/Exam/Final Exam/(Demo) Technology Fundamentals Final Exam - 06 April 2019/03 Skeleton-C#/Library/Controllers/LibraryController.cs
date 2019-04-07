1864a8552800fd80ec354ecd468795c1752c3ab6
KrastevIT <barcodebit000@gmail.com>
2019-04-07 09:57:21 +0300

    update Technology Fundamentals Final Exam - 06 April 2019

---
+++ 02.Technology Fundamentals with C# - January 2019/Exam/Final Exam/(Demo) Technology Fundamentals Final Exam - 06 April 2019/03 Skeleton-C#/Library/Controllers/LibraryController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Data;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db = new BookLibraryDbContext())
+            {
+                var library = db.Libraries.ToList();
+                return View(library);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            using (var db = new BookLibraryDbContext())
+            {
+                db.Libraries.Add(book);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new BookLibraryDbContext())
+            {
+                var libraryToEdit = db.Libraries.Find(id);
+                return View(libraryToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            using (var db = new BookLibraryDbContext())
+            {
+                db.Libraries.Update(book);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new BookLibraryDbContext())
+            {
+                var libraryDelete = db.Libraries.Find(id);
+                return View(libraryDelete);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+            using (var db = new BookLibraryDbContext())
+            {
+                db.Libraries.Remove(book);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 02.Technology Fundamentals with C# - January 2019/Exam/Final Exam/(Demo) Technology Fundamentals Final Exam - 06 April 2019/03 Skeleton-C#/Library/Data/BookLibraryDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Data
+{
+    public class BookLibraryDbContext : DbContext
+    {
+        public DbSet<Book> Libraries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=BookLibraryDb;Integrated Security=True;");
+        }
+    }
+}
