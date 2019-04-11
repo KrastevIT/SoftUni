5fbfc47b313784465417981129745465d6c2b21d
KrastevIT <barcodebit000@gmail.com>
2019-04-11 21:32:09 +0300

    add Retake Exam - 24 March 2019

---
+++ 02.Technology Fundamentals with C# - January 2019/Exam/Final Exam/Programming Fundamenta Additional Retake Exam - 24 March 2019/01 Bonus System/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01_Bonus_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfStudents = int.Parse(Console.ReadLine());
+            int countOfLectures = int.Parse(Console.ReadLine());
+            int bonus = int.Parse(Console.ReadLine());
+
+            double currentBonus = 0;
+            double maxBonus = 0;
+            double attendedLectures = 0;
+
+            for (int i = 0; i < countOfStudents; i++)
+            {
+                double attendancesOfStudent = double.Parse(Console.ReadLine());
+
+                currentBonus = attendancesOfStudent / countOfLectures * (5+ bonus);
+         
+                if (attendancesOfStudent > attendedLectures)
+                {
+                    maxBonus = currentBonus;
+                    attendedLectures = attendancesOfStudent;
+                }
+            }
+
+            Console.WriteLine($"The maximum bonus score for this course is {Math.Round(maxBonus)}.The student has attended {attendedLectures} lectures.");
+        }
+    }
+}
---
+++ 02.Technology Fundamentals with C# - January 2019/Exam/Final Exam/Programming Fundamenta Additional Retake Exam - 24 March 2019/02 Nascar Qualifications/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Nascar_Qualifications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = Console.ReadLine().Split();
+
+            var pilots = new List<string>(name);
+
+            string[] input = Console.ReadLine().Split();
+            while (input[0] != "end")
+            {
+                string command = input[0];
+                string racer = input[1];
+
+                if (command == "Race")
+                {
+                    if (!pilots.Contains(racer))
+                    {
+                        pilots.Add(racer);
+                    }
+                }
+                else if (command == "Accident")
+                {
+                    if (pilots.Contains(racer))
+                    {
+                        pilots.Remove(racer);
+                    }
+                }
+                else if (command == "Box")
+                {
+                    if (pilots.Contains(racer))
+                    {
+                        int index = pilots.IndexOf(racer);
+                        if (index == pilots.Count)
+                        {
+                            continue;
+                        }
+                        pilots.RemoveAt(index);
+                        pilots.Insert(index + 1, racer);
+                    }
+                }
+                else if (command == "Overtake")
+                {
+                    int position = int.Parse(input[2]);
+
+                    if (pilots.Contains(racer))
+                    {
+                        int index = pilots.IndexOf(racer);
+
+                        if (index +1 - position > 0)
+                        {
+                            pilots.RemoveAt(index);
+                            pilots.Insert(index - position, racer);
+                        }
+                    }
+                }
+
+                input = Console.ReadLine().Split();
+            }
+
+
+            Console.WriteLine(String.Join(" ~ ", pilots));
+
+
+        }
+    }
+}
