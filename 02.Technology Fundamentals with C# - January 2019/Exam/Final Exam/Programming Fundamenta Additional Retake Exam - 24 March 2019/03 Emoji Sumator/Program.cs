6c994aa8ec3bec13e624b937dd4471f70fee9b5e
KrastevIT <barcodebit000@gmail.com>
2019-04-12 11:45:31 +0300

    update Retake Exam - 24 March 2019

---
+++ 02.Technology Fundamentals with C# - January 2019/Exam/Final Exam/Programming Fundamenta Additional Retake Exam - 24 March 2019/03 Emoji Sumator/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _03_Emoji_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<int> emojiCode = Console.ReadLine().Split(":").Select(int.Parse).ToList();
+
+            List<string> emoji = new List<string>();
+            int totalPower = 0;
+            bool isEqual = false;
+
+            string pattern = @"(?<= )[:][a-z]{4,}[:](?=[ ,.!?])";
+
+            var matched = Regex.Matches(input, pattern);
+
+            foreach (Match item in matched)
+            {
+                emoji.Add(item.ToString());
+            }
+
+            for (int i = 0; i < emoji.Count; i++)
+            {
+                string currentEmoji = emoji[i];
+                var emojiSum = new List<int>();
+
+                for (int j = 0; j < currentEmoji.Length; j++)
+                {
+                    int currentChar = currentEmoji[j];
+                    int currentSum = 0;
+
+                    if (currentChar == ':')
+                    {
+                        continue;
+                    }
+
+                    currentSum += currentChar;
+                    emojiSum.Add(currentSum);
+                }
+
+                totalPower += emojiSum.Sum();
+
+                if (emojiSum.SequenceEqual(emojiCode))
+                {
+                    isEqual = true;
+                }
+            }
+
+            if (isEqual)
+            {
+                totalPower *= 2;
+            }
+
+            if (emoji.Count == 0)
+            {
+                Console.WriteLine($"Total Emoji Power: 0");
+            }
+            else
+            {
+                Console.WriteLine($"Emojis found: {string.Join(", ", emoji)}");
+                Console.WriteLine($"Total Emoji Power: {totalPower}");
+            }
+
+        }
+    }
+}
+
---
+++ 02.Technology Fundamentals with C# - January 2019/Exam/Final Exam/Programming Fundamenta Additional Retake Exam - 24 March 2019/04 International SoftUniada/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_International_SoftUniada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" -> ");
+
+            var countryAndPoints = new Dictionary<string, Dictionary<string, int>>();
+            var pointsCountry = new Dictionary<string, int>();
+
+            while (input[0] != "END")
+            {
+                string country = input[0];
+                string name = input[1];
+                int point = int.Parse(input[2]);
+
+                if (!countryAndPoints.ContainsKey(country))
+                {
+                    countryAndPoints[country] = new Dictionary<string, int>();
+                    pointsCountry[country] = 0;
+                }
+                if (!countryAndPoints[country].ContainsKey(name))
+                {
+                    countryAndPoints[country][name] = 0;
+                }
+
+                countryAndPoints[country][name] += point;
+                pointsCountry[country] += point;
+
+                input = Console.ReadLine().Split(" -> ");
+            }
+
+            foreach (var coutry in pointsCountry.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"{coutry.Key}: {coutry.Value}");
+
+                foreach (var kvp in countryAndPoints[coutry.Key])
+                {
+                    Console.WriteLine($" -- {kvp.Key} -> {kvp.Value}");
+                }
+            }
+        }
+    }
+}
