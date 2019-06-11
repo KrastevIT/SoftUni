3fc65630ccb2f12475fc86173c9ab599a1659a33
KrastevIT <barcodebit000@gmai.com>
2019-06-11 21:58:21 +0300

    Update Stacks and Queues - Lab

---
+++ C# Advanced - May 2019/Stacks and Queues - Lab/02 Stack Sum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+
+            int[] inputNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            foreach (var number in inputNumbers)
+            {
+                stack.Push(number);
+            }
+
+            string[] inputCommands = Console.ReadLine().ToLower().Split();
+
+            while (inputCommands[0] != "end")
+            {
+                string commad = inputCommands[0];
+                int index = int.Parse(inputCommands[1]);
+
+                if (commad == "add")
+                {
+                    int value = int.Parse(inputCommands[2]);
+
+                    stack.Push(index);
+                    stack.Push(value);
+
+                }
+                else if (commad == "remove")
+                {
+                    if (stack.Count >= index)
+                    {
+                        for (int i = 0; i < index; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+
+                inputCommands = Console.ReadLine().ToLower().Split();
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+
+
+        }
+    }
+}
---
+++ C# Advanced - May 2019/Stacks and Queues - Lab/05 Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var queue = new Queue<string>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (queue.Count > 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
