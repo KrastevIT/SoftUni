dcd249184f768a12a3806f7b36a25e8cf613b165
KrastevIT <barcodebit000@gmai.com>
2019-06-28 10:57:56 +0300

    Added Generics - Lab

---
+++ C# Advanced - May 2019/Generics - Lab/01 Box/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        List<T> boxes = new List<T>();
+
+        public int Count => boxes.Count();
+
+        public void Add(T element)
+        {
+            boxes.Add(element);
+        }
+
+        public T Remove()
+        {
+            if (Count > 0)
+            {
+                T lastElement = boxes.Last();
+
+                boxes.RemoveAt(Count - 1);
+
+                return lastElement;
+            }
+
+            throw new InvalidOperationException("Can not remove");
+        }
+    }
+}
