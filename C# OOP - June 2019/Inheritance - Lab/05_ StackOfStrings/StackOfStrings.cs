b7404c0af6681ac693c28a084d68e1bd551e53f3
KrastevIT <barcodebit000@gmai.com>
2019-07-05 19:25:25 +0300

    Added Inheritance - Lab

---
+++ C# OOP - June 2019/Inheritance - Lab/05_ StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        private StackOfStrings stack;
+
+        public StackOfStrings()
+        {
+
+        }
+
+        public bool IsEmpty()
+        {
+            if (stack == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void AddRange(List<string> items)
+        {
+            foreach (var item in items)
+            {
+                stack.Push(item);
+            }
+        }
+    }
+}
