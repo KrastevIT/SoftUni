32ce4728d8758ab960787a266c0361e21731ccda
KrastevIT <barcodebit000@gmai.com>
2019-07-21 23:20:17 +0300

    Added Polymorphism - Lab

---
+++ C# OOP - June 2019/Polymorphism - Lab/MathOperations/MathOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public double Add(double firstNum, double secondNum, double thirdNum)
+        {
+            return firstNum + secondNum + thirdNum;
+        }
+
+        public decimal Add(decimal firstNum, decimal secondNum, decimal thirdNum)
+        {
+            return firstNum + secondNum + thirdNum;
+        }
+    }
+}
---
+++ C# OOP - June 2019/Polymorphism - Lab/MathOperations/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            Console.WriteLine(math.Add(2, 3));
+            Console.WriteLine(math.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(math.Add(2.2m, 3.3m, 4.4m));
+        }
+    }
+}
