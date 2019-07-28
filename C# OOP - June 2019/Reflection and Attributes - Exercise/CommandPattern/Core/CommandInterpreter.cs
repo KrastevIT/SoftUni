afd2b0b8794f0e301820b581edcf4b99bd9aaa4b
KrastevIT <barcodebit000@gmai.com>
2019-07-28 12:17:02 +0300

    Added Reflection and Attributes - Exercise

---
+++ C# OOP - June 2019/Reflection and Attributes - Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,39 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string COMMAND_POSTFIX = "Command";
+        public string Read(string args)
+        {
+            string[] cmdTokens = args.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string commandName = cmdTokens[0] + COMMAND_POSTFIX;
+
+            string[] commandArgs = cmdTokens.Skip(1).ToArray();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            Type[] types = assembly.GetTypes();
+
+            Type typeToCreate = types.FirstOrDefault(t => t.Name == commandName);
+
+            if (typeToCreate == null)
+            {
+                throw new InvalidOperationException("Invalid Command Type!");
+            }
+
+            object instance = Activator.CreateInstance(typeToCreate);
+
+            ICommand command = (ICommand)instance;
+
+            string result = command.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
